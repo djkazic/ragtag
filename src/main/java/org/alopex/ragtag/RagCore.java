1c9ad6d4ae5e80b7f43f2079a51e74f760f39e79
Kevin Cai <kevincai96@gmail.com>
2016-01-23 11:21:32 -0500

    [ragtag] WorkerManager instantiation call

--- src/main/java/org/alopex/ragtag/RagCore.java
+++ src/main/java/org/alopex/ragtag/RagCore.java
@@ -1,13 +1,16 @@
 package org.alopex.ragtag;
 
 import org.alopex.ragtag.net.ServerNetworking;
+import org.alopex.ragtag.net.worker.WorkerManager;
 
 public class RagCore {
-	
+
+	public static WorkerManager wm;
 	private static ServerNetworking sn;
 	
 	public static void main(String[] args) {
 		Utilities.log("RagCore", "Initializing ragtag server...", false);
+		wm = new WorkerManager();
 		sn = new ServerNetworking();
 		sn.initialize();
 	}
