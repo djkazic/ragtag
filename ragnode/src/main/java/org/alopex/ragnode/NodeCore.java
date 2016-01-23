2661382b72e801db081b177410af0b83f7caaeb1
Kevin Cai <kevincai96@gmail.com>
2016-01-23 11:20:45 -0500

    [ragnode] Implementation of ID skeleton

--- ragnode/src/main/java/org/alopex/ragnode/NodeCore.java
+++ ragnode/src/main/java/org/alopex/ragnode/NodeCore.java
@@ -3,10 +3,24 @@ package org.alopex.ragnode;
 public class NodeCore {
 	
 	private static ClientNetworking cn;
+	private static String id;
 	
     public static void main(String[] args) {
         Utilities.log("RagNodeCore", "Initializing ragtag node...", false);
         cn = new ClientNetworking();
 		cn.initialize();
+		generateId();
+    }
+    
+    public static ClientNetworking getClientNetworking() {
+    	return cn;
+    }
+    
+    public static String getId() {
+    	return id;
+    }
+    
+    private static void generateId() {
+    	//TODO: set ID here
     }
 }
