583092799e034e157090abeadb0732930f2738ba
Kevin Cai <kevincai96@gmail.com>
2016-01-23 11:05:37 -0500

    Added override methods for ClientListener

--- ragnode/src/main/java/org/alopex/ragnode/ClientListener.java
+++ ragnode/src/main/java/org/alopex/ragnode/ClientListener.java
@@ -1,7 +1,17 @@
 package org.alopex.ragnode;
 
+import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
 public class ClientListener extends Listener {
+	
+	@Override
+	public void connected(Connection connection) {
+		Utilities.log(this, "Connected to server, ID" + connection.getID(), false);
+	}
 
+	@Override
+	public void received(Connection connection, Object obejct) {
+		Utilities.log(this, "Received packet on connection ID " + connection.getID(), false);
+	}
 }
