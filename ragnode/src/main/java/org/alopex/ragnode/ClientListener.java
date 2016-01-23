ba9f7f7f51d15da9fb36b21c22280c2a547bf3b3
Kevin Cai <kevincai96@gmail.com>
2016-01-23 11:19:41 -0500

    [ragnode] Set NetRequest processor

--- ragnode/src/main/java/org/alopex/ragnode/ClientListener.java
+++ ragnode/src/main/java/org/alopex/ragnode/ClientListener.java
@@ -1,5 +1,8 @@
 package org.alopex.ragnode;
 
+import org.alopex.ragnode.net.packets.NetData;
+import org.alopex.ragnode.net.packets.NetRequest;
+
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 
@@ -11,7 +14,11 @@ public class ClientListener extends Listener {
 	}
 
 	@Override
-	public void received(Connection connection, Object obejct) {
-		Utilities.log(this, "Received packet on connection ID " + connection.getID(), false);
+	public void received(Connection connection, Object object) {
+		if(object instanceof NetRequest) {
+			NetRequest.processRequest(connection, object);
+		} else if(object instanceof NetData) {
+			NetData.processData(connection, object);
+		}
 	}
 }
