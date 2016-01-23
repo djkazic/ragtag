19e08b1eb81d4bc5b7f11e171726b8cd98235fcc
Kevin Cai <kevincai96@gmail.com>
2016-01-23 11:05:51 -0500

    Set valid Config test server (subject to change)

--- ragnode/src/main/java/org/alopex/ragnode/Config.java
+++ ragnode/src/main/java/org/alopex/ragnode/Config.java
@@ -4,6 +4,6 @@ public class Config {
 
 	public static final int BUFFER_SIZE = 512000 * 4;
 	public static final int BIND_PORT = 17380;
-	public static final String SERVER_IP = "0.0.0.0";
+	public static final String SERVER_IP = "127.0.0.1";
 	
 }
