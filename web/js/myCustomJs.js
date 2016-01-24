25b160c7ef86d24e672e5d686fdab7c9dd3a64b7
Brittany Chiang <brittany.chiang@gmail.com>
2016-01-24 13:03:38 -0500

    button works

--- web/js/myCustomJs.js
+++ web/js/myCustomJs.js
@@ -4,40 +4,49 @@ var updateFreq = 5; //In seconds
 
 //});
 $(document).ready(
-function() {
-  updateNumWorkers();
-  var dataarr = [1,2,3,5];
-  d3.select("#workers-benchmark")
-    .selectAll("div")
-      .data(dataarr)
-    .enter().append("div")
-      .style("height", function(d) { return d*10 + "px"; })
-      .style("class", function(d) { return d + "th-child"; });
-});
+    function() {
+        updateNumWorkers();
+        var dataarr = [1, 2, 3, 5];
+        d3.select("#workers-benchmark")
+            .selectAll("div")
+            .data(dataarr)
+            .enter().append("div")
+            .style("height", function(d) {
+                return d * 10 + "px";
+            })
+            .style("class", function(d) {
+                return d + "th-child";
+            });
+    });
 
 function updateWorkerChart() {
+    d3.select("#workers-benchmark").data([6]).append("div")
+        .style("height", function(d) {
+            return d * 10 + "px";
+        });
 
-  console.log($('#workers-benchmark.1th-child'));
-  d3.select("#workers-benchmark").data([6]).append("div")
-    .style("height", function(d) { return d*10 + "px"; });
+    $("#workers-benchmark").children().first().remove();
     console.log("button pressed");
+    console.log($('#workers-benchmark'));
 }
+
 function getLargest(data) {
-  var max = -9999999;
-  for(var i = 0; i < data.length; i++) {
-    if(data[i] > max)
-    {
-      max = data[i];
+    var max = -9999999;
+    for (var i = 0; i < data.length; i++) {
+        if (data[i] > max) {
+            max = data[i];
+        }
     }
-  }
 }
+
 function updateNumWorkers() {
-  $.post('localhost:8888/api', '{ "rpc": "num_workers" }')
-    .done( function(newNumber) {
-      $('#workers-number').html(1);})
-    .fail( function() {
-      $('#workers-number').html(-1);
-  });
+    $.post('localhost:8888/api', '{ "rpc": "num_workers" }')
+        .done(function(newNumber) {
+            $('#workers-number').html(1);
+        })
+        .fail(function() {
+            $('#workers-number').html(-1);
+        });
 }
 
 
